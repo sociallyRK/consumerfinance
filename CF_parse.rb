cb080521a17e161be44ba034d12d892f604fd382
RahulKhanna <sociallyrk@gmail.com>
2014-11-04 16:22:35 -0800

    JSON Parse of any link

--- CF_parse.rb
+++ CF_parse.rb
@@ -2,7 +2,7 @@ require 'typhoeus'
 require 'json'
 require 'csv'
 
-response = Typhoeus.get("https://api.consumerfinance.gov/data/hmda/slice/hmda_lar.json?#!/property_type=1,2&action_taken=1&select=as_of_year,loan_purpose_name,count&section=summary")
+response = Typhoeus.get("https://api.consumerfinance.gov/data/hmda/slice/hmda_lar.json?#!/lien_status=1&loan_purpose=1&action_taken=1&select=as_of_year,loan_type_name,count&section=summary")
 result = JSON.parse(response.body)
 key_array = []
 value_array = []
@@ -10,9 +10,8 @@ value_array = []
 CSV.open("ConsumerFinance.csv", 'w') do  |csv| 
   result["results"][0].each_key { |key|
     key_array << key }
-
     csv << key_array
-
+    
   result["results"].each do |element|
     key_array.each do |key|
       if element[key].nil?
